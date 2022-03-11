efedaf2cc927252cf8a42a751ecf9ca221d80b51
Caio F <silvacaio295@gmail.com>
2022-03-11 14:06:03 -0300

    finishing news section

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+  'x-rapidapi-key': 'fa5d9c6928msh5b9d73bb1954fb2p1cad9ejsn3ff31bdd87c2'
+}
