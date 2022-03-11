c20faf5012ec06c0f57f7369f006380c30e362f7
Caiote <silvacaio295@gmail.com>
2022-03-11 09:01:49 -0300

    second commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'fa5d9c6928msh5b9d73bb1954fb2p1cad9ejsn3ff31bdd87c2'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins')
+    })
+  })
+})
+
+export const { useGetCryptosQuery } = cryptoApi
