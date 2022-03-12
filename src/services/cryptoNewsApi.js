8d2e9259267726b758240bf1022994d179e4645a
Caiote <silvacaio295@gmail.com>
2022-03-11 22:07:12 -0300

    error

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -5,3 +5,22 @@ const cryptoNewsHeaders = {
   'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
   'x-rapidapi-key': 'fa5d9c6928msh5b9d73bb1954fb2p1cad9ejsn3ff31bdd87c2'
 }
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        )
+    })
+  })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi
